975b5fcbf7bae49bc9079250242c44509075ff7d
soyvitou2019 <9ball2019@gmail.com>
2024-03-30 09:35:20 +0700

    register part

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import re
 import threading
 import tkinter as tk
 from tkinter import *
@@ -9,6 +10,8 @@ import ttkbootstrap as ttk
 import cv2
 from PIL import Image, ImageTk
 import dlib
+from features_extraction_to_csv import main
+from mysql_query import MysqlQuery
 
 class AttendanceTracking:
     def __init__(self, root):
@@ -17,6 +20,9 @@ class AttendanceTracking:
         self.root = root
         self.root.title("Attendance Tracking System")
         self.root.geometry("1080x500")
+
+        # Mysql configure
+        self.mysql_connection = MysqlQuery(host="47.129.17.36", user="vitou_wct", password="vitou2357.!", database="attendance_test1")
 
         # Frame Variable
         self.switch_frame = ttk.Frame(self.root)
@@ -44,6 +50,31 @@ class AttendanceTracking:
         self.reclassify_interval_cnt = 0
         self.reclassify_interval = 10
 
+        # Register Variable
+        self.current_face_dir = ""
+        self.existing_faces_cnt = 0 # cnt for counting saved faces
+        self.input_name_char = ""
+        self.current_frame_faces_cnt = 0  #  cnt for counting faces in current frame
+        self.path_photos_from_camera = "data/data_faces_from_camera/"
+        self.out_of_range_flag = False
+        self.face_folder_created_flag = False
+        self.ss_cnt = 0   #  cnt for screen shots
+        # Current frame and face ROI position
+        self.current_frame = np.ndarray
+        self.face_ROI_image = np.ndarray
+        self.face_ROI_width_start = 0
+        self.face_ROI_height_start = 0
+        self.face_ROI_width = 0
+        self.face_ROI_height = 0
+        self.ww = 0
+        self.hh = 0
+        # GUI part
+        self.frame_right_info = tk.Frame(self.root)
+        self.label_warning = tk.Label(self.frame_right_info)
+        self.label_face_cnt = tk.Label(self.frame_right_info, text="Faces in current frame: ")
+        self.log_all = tk.Label(self.frame_right_info)
+
+
 
         # Face Recognition Function
         self.video_capture = cv2.VideoCapture(0)
@@ -54,7 +85,7 @@ class AttendanceTracking:
         # Init Function & Process
         self.get_face_database()
         self.wraper_switch_ui()
-        self.draw_left_frame_camera()
+        self.process()
 
     def get_face_database(self):
         if os.path.exists("data/features_all.csv"):
@@ -110,6 +141,7 @@ class AttendanceTracking:
     def switch_mode(self):
         self.recognition_mode = not self.recognition_mode
         if self.recognition_mode:
+            self.get_face_database()
             self.recognition_UI_frame = ttk.Frame(self.root)
             self.destroy_register_ui()
             self.draw_right_part_recognition_UI()
@@ -119,29 +151,29 @@ class AttendanceTracking:
             self.draw_right_part_register_info_ui()
 
     def update_image_recognition(self):
-        if self.name:
-            path = "./data/data_faces_from_camera"
-            dir_list = os.listdir(path)
-            for i in range(len(dir_list)):
-                if f'person_{i}_{self.name}' in dir_list:
-                    image_path = f'./data/data_faces_from_camera/person_{i}_{self.name}/img_face_2.jpg'
-         # Load the original image
-        original_img = Image.open(image_path)
-        original_img = original_img.resize((200, 200), Image.ANTIALIAS)  # Resize the image if necessary
+        if self.img_label.winfo_exists():
+            image_path = "./images/fake_profile.png"
+            if self.name:
+                dir_list = os.listdir(self.path_photos_from_camera)
+                if self.name in dir_list:
+                    image_path = f'./data/data_faces_from_camera/{self.name}/img_face_2.jpg'
+            # Load the original image
+            original_img = Image.open(image_path)
+            original_img = original_img.resize((200, 200), Image.ANTIALIAS)  # Resize the image if necessary
 
-        # Load the frame image
-        frame_img = Image.open("./images/frame.png")  # Replace "frame.png" with the path to your frame image
-        frame_img = frame_img.resize((200, 200), Image.ANTIALIAS)  # Resize the frame image to match the original image size
+            # Load the frame image
+            frame_img = Image.open("./images/frame.png")  # Replace "frame.png" with the path to your frame image
+            frame_img = frame_img.resize((200, 200), Image.ANTIALIAS)  # Resize the frame image to match the original image size
 
-        # Create a new image with the frame
-        img_with_frame = Image.new("RGBA", (200, 200), (255, 255, 255, 0))  # Create a transparent image
-        img_with_frame.paste(original_img, (0, 0))  # Paste the original image onto the transparent image
-        img_with_frame.paste(frame_img, (0, 0), mask=frame_img)  # Paste the frame image onto the transparent image
+            # Create a new image with the frame
+            img_with_frame = Image.new("RGBA", (200, 200), (255, 255, 255, 0))  # Create a transparent image
+            img_with_frame.paste(original_img, (0, 0))  # Paste the original image onto the transparent image
+            img_with_frame.paste(frame_img, (0, 0), mask=frame_img)  # Paste the frame image onto the transparent image
 
-        # Convert the new image to PhotoImage
-        img_with_frame_tk = ImageTk.PhotoImage(img_with_frame)
-        self.img_label.configure(image=img_with_frame_tk)
-        self.img_label.image = img_with_frame_tk
+            # Convert the new image to PhotoImage
+            img_with_frame_tk = ImageTk.PhotoImage(img_with_frame)
+            self.img_label.configure(image=img_with_frame_tk)
+            self.img_label.image = img_with_frame_tk
 
     def draw_right_part_recognition_UI(self):
         self.recognition_UI_frame.place(relx=0.68, rely=0.2, relwidth=0.2, relheight=0.6)
@@ -179,11 +211,15 @@ class AttendanceTracking:
         submit_btn = ttk.Button(self.recognition_UI_frame,
                                 text="Submit",
                                 bootstyle = "success, outline",
-                                command=self.prediction)
+                                command=self.submit_data_into_database)
         submit_btn.pack(side='bottom', fill="both", expand=True)
         
     def update_display_name(self):
-        self.user_name_btn.configure(text=f'UserName: {self.name}')
+        if self.user_name_btn.winfo_exists():
+            self.user_name_btn.configure(text=f'UserName: {self.name}')
+
+    def submit_data_into_database(self):
+        self.mysql_connection.write_data_into_attendance(self.name)
 
 
     def draw_right_part_register_info_ui(self):
@@ -197,11 +233,11 @@ class AttendanceTracking:
                                         font=15, 
                                         bootstyle="success")
         input_name_btn = ttk.Label(self.register_ui_frame, 
-                                        text = "Name", 
+                                        text = "Full-Name: Soy Vitou", 
                                         font=10, 
                                         bootstyle="success")
         
-        input_name2_btn = ttk.Entry(self.register_ui_frame, 
+        self.input_name2_btn = ttk.Entry(self.register_ui_frame, 
                                         textvariable= "Input username",
                                         width=13,
                                         font=10, 
@@ -212,23 +248,36 @@ class AttendanceTracking:
                                         font=15, 
                                         bootstyle="success")
         save_current_face_btn = ttk.Button(self.register_ui_frame, 
-                                        text = "Save Current Face", 
+                                        text = "Save Current Face",
+                                        command=self.save_current_face,
+                                        bootstyle="success")
+        compile_feature_btn = ttk.Button(self.register_ui_frame, 
+                                        text = "Register",
+                                        command=self.register_feature_fn,
                                         bootstyle="success")
         
         header_register_btn.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
         step1_input_name_btn.grid(row=1, column=0, padx=10, pady=10)
         input_name_btn.grid(row=2, column=0, pady=10)
-        input_name2_btn.grid(row=2, column=1, pady=10)
+        self.input_name2_btn.grid(row=2, column=1, pady=10)
         step2_input_save_btn.grid(row=3, column=0, padx=10, pady=10)
         save_current_face_btn.grid(row=4, column=0, columnspan=3, padx=10, pady=10)
-        
+        compile_feature_btn.grid(row=5, column=0, columnspan=3, padx=10, pady=10)
+
+    def register_feature_fn(self):
+        main_thread = threading.Thread(target=main)
+        main_thread.start()
+
     def destroy_register_ui(self):
         self.register_ui_frame.destroy()
     
     def destroy_face_recognition_ui(self):
         self.recognition_UI_frame.destroy()
 
-    
+    def get_name_from_entry(self):
+        input_value = self.input_name2_btn.get()
+        self.input_name_char = input_value.replace(" ", '-').lower()
+        # print(self.input_name_char)
     
     def prediction(self, faces, img_rd):
         # 6.1  if cnt not changes
@@ -313,12 +362,60 @@ class AttendanceTracking:
                         # Insert attendance record
                         nam =self.face_name_known_list[similar_person_num]
                         self.name = nam
-                        self.update_image_recognition()
-                        self.update_display_name()
 
-    def draw_left_frame_camera(self):
+
+    # Face Register Part
+
+    def create_face_folder(self):
+        #  Create the folders for saving faces
+        self.get_name_from_entry()
+        self.existing_faces_cnt += 1
+        if self.input_name_char:
+            self.current_face_dir = self.path_photos_from_camera + self.input_name_char
+        else:
+            self.current_face_dir = self.path_photos_from_camera
+        os.makedirs(self.current_face_dir)
+
+        self.ss_cnt = 0  #  Clear the cnt of screen shots
+        self.face_folder_created_flag = True  # Face folder already created
+        
+    def save_current_face(self):
+        self.get_name_from_entry()
+        filenames = os.listdir(self.path_photos_from_camera)
+        already_created = False
+        if self.input_name_char in filenames:
+            already_created = True
+        if not already_created:
+            self.create_face_folder()
+  
+        if self.current_frame_faces_cnt == 1:
+            if not self.out_of_range_flag:
+                self.ss_cnt += 1
+                #  Create blank image according to the size of face detected
+                self.face_ROI_image = np.zeros((int(self.face_ROI_height * 2), self.face_ROI_width * 2, 3),
+                                                np.uint8)
+                for ii in range(self.face_ROI_height * 2):
+                    for jj in range(self.face_ROI_width * 2):
+                        self.face_ROI_image[ii][jj] = self.current_frame[self.face_ROI_height_start - self.hh + ii][
+                            self.face_ROI_width_start - self.ww + jj]
+                self.face_ROI_image = cv2.cvtColor(self.face_ROI_image, cv2.COLOR_BGR2RGB)
+
+                cv2.imwrite(self.current_face_dir + "/img_face_" + str(self.ss_cnt) + ".jpg", self.face_ROI_image)
+                
+            else:
+                self.log_all["text"] = "Please do not out of range!"
+        else:
+            self.log_all["text"] = "No face in current frame!"
+
+
+
+
+
+
+    def process(self):
         ret, frame = self.video_capture.read()
         img_rd = frame
+        self.current_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         if ret:
             # Recognition
             faces = self.detector(frame, 0)
@@ -328,6 +425,20 @@ class AttendanceTracking:
 
             
             if self.recognition_mode:
+                # refresh recognition image
+                current_time = datetime.datetime.now()
+                time_difference = current_time - self.previous_time
+                threshold = datetime.timedelta(seconds=5)
+                if self.current_frame_face_cnt > 0:
+                    self.previous_time = current_time
+                if time_difference > threshold:
+                    self.name = None
+                    self.update_display_name()
+                    self.img_label.configure(image=self.save_fake_icons_tk)
+                    self.img_label.image = self.save_fake_icons_tk
+                    print("5 seconds have passed.")
+                    self.previous_time = current_time
+
                 # 3.  Update cnt for faces in frames
                 self.last_frame_face_cnt = self.current_frame_face_cnt
                 self.current_frame_face_cnt = len(faces)
@@ -341,6 +452,40 @@ class AttendanceTracking:
 
                 result_threading = threading.Thread(target=self.prediction, args=(faces, img_rd))
                 result_threading.start()
+                if not result_threading.is_alive() and self.name:
+                    self.update_image_recognition()
+                    self.update_display_name()
+            else:
+                self.label_face_cnt["text"] = str(len(faces))
+                #  Face detected
+                if len(faces) != 0 and len(faces) < 2:
+                    #   Show the ROI of faces
+                    for k, d in enumerate(faces):
+                        self.face_ROI_width_start = d.left()
+                        self.face_ROI_height_start = d.top()
+                        #  Compute the size of rectangle box
+                        self.face_ROI_height = (d.bottom() - d.top())
+                        self.face_ROI_width = (d.right() - d.left())
+                        self.hh = int(self.face_ROI_height / 2)
+                        self.ww = int(self.face_ROI_width / 2)
+
+                        # If the size of ROI > 480x640
+                        if (d.right() + self.ww) > 640 or (d.bottom() + self.hh > 480) or (d.left() - self.ww < 0) or (
+                            d.top() - self.hh < 0):
+                            self.label_warning["text"] = "OUT OF RANGE"
+                            self.label_warning['fg'] = 'red'
+                            self.out_of_range_flag = True
+                            color_rectangle = (255, 0, 0)
+                        else:
+                            self.out_of_range_flag = False
+                            self.label_warning["text"] = ""
+                            color_rectangle = (255, 255, 255)
+                        self.current_frame = cv2.rectangle(self.current_frame,
+                                                        tuple([d.left() - self.ww, d.top() - self.hh]),
+                                                        tuple([d.right() + self.ww, d.bottom() + self.hh]),
+                                                        color_rectangle, 2)
+                #   Show the ROI of faces
+                self.current_frame_faces_cnt = len(faces)
                         
             # Display the updated frame
             frame = cv2.cvtColor(img_rd, cv2.COLOR_BGR2RGB)
@@ -351,18 +496,7 @@ class AttendanceTracking:
             # Keep a reference to prevent garbage collection
             self.video_frame.image = frame  
         
-        current_time = datetime.datetime.now()
-        time_difference = current_time - self.previous_time
-        threshold = datetime.timedelta(seconds=7)
-        if self.current_frame_face_cnt > 0:
-            self.previous_time = current_time
-        if time_difference > threshold:
-            self.name = None
-            self.update_display_name()
-            self.img_label.configure(image=self.save_fake_icons_tk)
-            self.img_label.image = self.save_fake_icons_tk
-            print("5 seconds have passed.")
-            self.previous_time = current_time
+        
             
         # Update every 10 milliseconds 
         self.root.after(10, self.update_video)  
@@ -378,10 +512,12 @@ class AttendanceTracking:
         self.video_frame.place(width=560, height=470, relx=0.03, rely=0.03)
 
     def update_video(self):
-        self.draw_left_frame_camera()
+        self.process()
 
     def main(self):
         self.root.mainloop()
+
+
 
 root = ttk.Window(themename="superhero")
 app = AttendanceTracking(root)
---
+++ mysql_query.py
@@ -0,0 +1,65 @@
+import mysql.connector
+import datetime
+
+class MysqlQuery:
+    def __init__(self, host, user, password, database):
+        self.conn = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+
+    def write_data_into_users(self, data):
+        cursor = self.conn.cursor()
+        insert_query = "INSERT INTO users (username, email, password, shift_id) VALUES (%s, %s, %s, %s)"
+        cursor.execute(insert_query, data)
+        self.conn.commit()
+        print("successfully")
+
+    def check_in_or_out(self, user_id):
+        current_date = datetime.datetime.now().date().strftime('%Y-%m-%d')
+        cursor = self.conn.cursor()
+        period = ['00:00:00', '24:00:00']
+        query = "SELECT check_status FROM attendances WHERE employee_id = %s AND DATE(created_at) = %s AND time(created_at) BETWEEN %s AND %s ORDER BY created_at DESC LIMIT 1"
+        cursor.execute(query, (user_id, current_date, period[0], period[1]))
+        check_status = cursor.fetchone()
+        if check_status:
+            return check_status[0]
+        else:
+            return None
+        
+    def get_user_id_by_username(self, username):
+        cursor = self.conn.cursor()
+        query = "SELECT id, shift_id FROM employees WHERE CONCAT(LOWER(first_name), '-', LOWER(last_name)) = %s"
+        cursor.execute(query, (username,))  
+        user_data = cursor.fetchone()
+        if user_data:
+            user_id, shift_id = user_data
+            return user_id, shift_id
+        else:
+            return None, None
+        
+    def write_data_into_attendance(self, name):
+        employee_id, shift_id = self.get_user_id_by_username(username=name)
+        # shift_id = int(shift_id)
+        check_status = self.check_in_or_out(user_id=employee_id)
+        print(check_status)
+        if check_status == None:
+            check_status = 0
+        else:
+            check_status = not check_status
+
+        cursor = self.conn.cursor()
+        data = [employee_id, check_status]
+        insert_query = "INSERT INTO attendances (employee_id, check_status) VALUES (%s, %s)"
+        cursor.execute(insert_query, data)
+        self.conn.commit()
+        print("successfully")
+
+    def close_connection(self):
+        if self.conn.is_connected():
+            self.conn.close()
+            print("Connection closed.")
+        else:
+            print("No active connection to close.")
